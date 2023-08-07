fe1732fb6e43a986a74e9c114ee4aa2da942e674
Dávid Bolvanský <david.bolvansky@gmail.com>
2023-08-07 15:58:28 +0200

    remove old bad tricks

--- runner/include/AttackBenchmark.hpp
+++ runner/include/AttackBenchmark.hpp
@@ -54,84 +54,10 @@ void AttackBenchmark<BaseAttack>::addSpecificArguments() {
 	this->addArgument("30");
 }
 
-struct PasswordLenCount
-{
-	size_t len;
-	size_t count;
-};
-
-inline std::istream &operator>>(std::istream &in, PasswordLenCount &data)
-{
-	in>>data.len;
-	Logging::debugPrint(Logging::Detail::DevelDebug, "data len "+RunnerUtils::toString(data.len));
-	if(in.get() != ':')
-	{
-		in.unget();
-		in.setstate(std::ios::failbit);
-	}
-	in>>data.count;
-	Logging::debugPrint(Logging::Detail::DevelDebug, "data count "+RunnerUtils::toString(data.count));
-	if(in.get() != ';')
-	{
-		in.unget();
-		in.setstate(std::ios::failbit);
-	}
-	return in;
-}
-
-static const unsigned bufSize = 16384;
-
 template <typename BaseAttack>
-std::string AttackBenchmark<BaseAttack>::addRequiredFile(const std::string &file_name)
-{
-	std::string shape;
-	if((file_name == "dict1" || file_name == "dict2") && this->config_.find("benchmark_"+file_name, shape))
-	{
-		Logging::debugPrint(Logging::Detail::CustomOutput, "Making benchmark dictionary named "+file_name);
-		File resolvedFile;
-		if(!this->directory_.find(file_name, resolvedFile))
-		{
-			RunnerUtils::runtimeException("Missing hashcat required file " + file_name);
-		}
-		std::ofstream dict(resolvedFile.getRelativePath().c_str(), std::ios::binary);
-		std::istringstream convertor(shape);
-		PasswordLenCount pwdLenCount;
-		time_t startTime = time(NULL);
-		bool running = true;
-		while(running && convertor>>pwdLenCount)
-		{
-			//we don't really expect passwords that long, but better safe than sorry
-			//-1 for the newline
-			if(pwdLenCount.len > bufSize-1)
-			{
-				RunnerUtils::runtimeException("Password too long");
-			}
-			char buf[bufSize];
-			//create the "password"
-			std::memset(buf, 'a', pwdLenCount.len);
-			buf[pwdLenCount.len] = '\n';
-			//copy it into the whole buffer to speed up writing
-			size_t repetitions = std::min(bufSize/(pwdLenCount.len+1), pwdLenCount.count);
-			size_t lineLen = pwdLenCount.len+1;
-			for(size_t i = 1; i < repetitions; ++i)
-			{
-				std::memcpy(buf+i*lineLen, buf, lineLen);
-			}
-			//do the writes
-			while(running && pwdLenCount.count)
-			{
-				size_t linesToWrite = std::min(repetitions, pwdLenCount.count);
-				dict.write(buf, lineLen*linesToWrite);
-				pwdLenCount.count -= linesToWrite;
-				running = time(NULL)-startTime < DICT_GEN_TIME_LIMIT;
-			}
-		}
-		if(!running)
-		{
-			Logging::debugPrint(Logging::Detail::CustomOutput, "Generating interrupted due to passed time limit");
-		}
-	}
-	return BaseAttack::addRequiredFile(file_name);
+std::string
+AttackBenchmark<BaseAttack>::addRequiredFile(const std::string &file_name) {
+        return BaseAttack::addRequiredFile(file_name);
 }
 
 #endif // ATTACKBENCHMARK_HPP
